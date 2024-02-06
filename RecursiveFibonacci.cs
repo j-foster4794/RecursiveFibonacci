1c499bc3b7fd1dd7cea369e4083a8258fde79e83
j-foster4794 <j.foster4794@gmail.com>
2024-02-06 16:33:37 +0000

    first commit

---
+++ RecursiveFibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Xml.XPath;
+
+namespace RecursiveFibonnaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("What number of the fibonacci sequence do you want?");
+            int position = Convert.ToInt32(Console.ReadLine());
+            int result = fib(position);
+            System.Console.WriteLine("Position " + position + " is " + result + ".");
+        }
+
+        static int fib(int x){
+            if (x == 0)
+            {
+                return 0;
+            }
+            else if (x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fib(x-1) + fib(x-2);
+            }
+        }
+    }
+}
